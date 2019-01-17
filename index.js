a0217620c91af65492ff2ace42e3531a6cc65c91
lucky-reducer-8961 <mshin1995@gmail.com>
2019-01-17 05:43:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,9 +14,9 @@ function sayHiToGrandma(string) {
   if (string.toLowerCase() === string)
   {return "I can\'t hear you!"}
   else
-  if (string.toUpperCase() === string)
+  (string.toUpperCase() === string)
   {return "YES INDEED!"}
   else
-  if (string === "I love you, Grandma.")
+  (string === "I love you, Grandma.")
   {return "I love you, too."}
 }
